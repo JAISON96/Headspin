e570c9be401710120cc8a4e5dfa57d99e9815e90
JAISON96 <65943024+JAISON96@users.noreply.github.com>
2020-05-26 21:51:27 +0530

    Update print_prime.py

--- print_prime.py
+++ print_prime.py
@@ -5,16 +5,17 @@ def primenumber(fp,lp):
         count = 0
         i = 2
     
-        while(i <= number//2):
+        while(i <= number):
             if(number % i == 0):
                 count = count + 1
-                break
+                
             i = i + 1
 
-        if (count == 0 and number != 1):
+        if (count ==1 and number != 1):
             print(" %d" %number, end = '  ')
         number = number  + 1
 
 fp1 = int(input(" Please enter the first limit: "))
 lp1 = int(input(" Please enter the last limit: "))
 primenumber(fp1,lp1)
+
